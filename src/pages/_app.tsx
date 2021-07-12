4241451045d7499c6525c12a3d063abd8f87e990
Natasha Freitas <natasha.freitas@acct.global>
2021-07-11 22:04:36 -0300

    podcastr

---
+++ src/pages/_app.tsx
@@ -0,0 +1,22 @@
+import "../styles/global.scss";
+import styles from "../styles/app.module.scss";
+
+import { PlayerContextProvider } from "../contexts/PlayerContext";
+import { Header } from "../components/Header";
+import { Player } from "../components/Player";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <PlayerContextProvider>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContextProvider>
+  );
+}
+
+export default MyApp;
